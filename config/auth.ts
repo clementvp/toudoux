5382f1d5a62a215298995be463ac73c220cfe13e
clementvp <clement.vanpeuter@gmail.com>
2026-01-10 01:35:26 +0100

    initialize project structure with essential configurations and components

---
+++ config/auth.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from '@adonisjs/auth'
+import { sessionGuard, sessionUserProvider } from '@adonisjs/auth/session'
+import type { InferAuthenticators, InferAuthEvents, Authenticators } from '@adonisjs/auth/types'
+import { tokensGuard, tokensUserProvider } from '@adonisjs/auth/access_tokens'
+
+const authConfig = defineConfig({
+  default: 'web',
+  guards: {
+    web: sessionGuard({
+      useRememberMeTokens: false,
+      provider: sessionUserProvider({
+        model: () => import('#models/user'),
+      }),
+    }),
+
+    api: tokensGuard({
+      provider: tokensUserProvider({
+        model: () => import('#models/user'),
+        tokens: 'accessTokens', // Doit correspondre à la prop dans ton modèle User
+      }),
+    }),
+  },
+})
+
+export default authConfig
+
+/**
+ * Inferring types from the configured auth
+ * guards.
+ */
+declare module '@adonisjs/auth/types' {
+  export interface Authenticators extends InferAuthenticators<typeof authConfig> {}
+}
+declare module '@adonisjs/core/types' {
+  interface EventsList extends InferAuthEvents<Authenticators> {}
+}
